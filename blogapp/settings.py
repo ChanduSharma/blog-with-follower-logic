e598779ba94459989968bff09b42949c5bd8163e
chandu <chandu.rikku@gmail.com>
2019-01-14 10:23:49 +0530

    Project Initiation

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ff29m95%lro05*)&0!dn&df!1c5n=*%5_9p@*y+z9iy6yz3jj6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-in'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
